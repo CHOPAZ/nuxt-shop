48400ff5d3857f89f644f0d5618e147974c57d23
Pavel <paxa627894@yandex.ru>
2026-03-16 23:15:05 +0500

    update: added error page and styled

---
+++ app/components/VButton.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const { color = "primary" } = defineProps<{
+  color?: "primary" | "ghost";
+}>();
+</script>
+
+<template>
+  <button :class="color">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+button {
+  padding: 16px;
+  font-weight: 700;
+  cursor: pointer;
+  border-radius: 4px;
+  border: 1px solid var(--color-black);
+}
+.primary {
+  background: var(--color-black);
+  color: var(--color-white);
+}
+
+.primary:hover {
+  background: var(--color-black-light);
+}
+
+.ghost {
+  background: var(--color-white);
+  color: var(--color-black);
+}
+
+.ghost:hover {
+  background: var(--color-black-light);
+  color: var(--color-white);
+}
+</style>
---
+++ app/error.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+import VButton from "./components/VButton.vue";
+
+const { error } = defineProps<{ error: NuxtError }>();
+
+function goIndexPage() {
+  navigateTo("/");
+}
+</script>
+
+<template>
+  <div class="error">
+    <h1>{{ error.status }} ошибка</h1>
+    <div v-if="error.status === 404" class="error__message">
+      Страница не найдена, попробуйте перейти на главную страницу
+    </div>
+    <div v-else class="error__message">{{ error.statusText }}</div>
+    <VButton color="ghost" @click="goIndexPage">Главная страница</VButton>
+  </div>
+</template>
+
+<style scoped>
+.error {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+  height: 100vh;
+}
+.error__message {
+  color: var(--color-dark-grey);
+  text-align: center;
+  font-family: var(--font-DM-Sans);
+  font-size: 20px;
+  line-height: 26px;
+  margin-bottom: 40px;
+  max-width: 400px;
+}
+</style>
