0f779a8387ee1ad8ec683df1a0713431cd147b70
Pavel <paxa627894@yandex.ru>
2026-03-23 23:02:25 +0500

    update: settings icon module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/fonts',
-    '@nuxt/scripts'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/fonts",
+    "@nuxt/scripts",
+    "@nuxt/icon",
+  ],
+  icon: {
+    customCollections: [
+      {
+        prefix: "icons",
+        dir: "./app/assets/icons",
+      },
+    ],
+  },
+});
