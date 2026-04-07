bcfdfd4a1781b44f8cde068f5ada276a94257966
Pavel <paxa627894@yandex.ru>
2026-04-07 22:58:52 +0500

    update: create card product, interface product, and get categories

---
+++ app/components/CatalogCard.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import type { IProduct } from "~/interfaces/product.interface";
+
+const config = useRuntimeConfig();
+const product = defineProps<IProduct>();
+</script>
+
+<template>
+  <NuxtLink to="/" class="card">
+    <div
+      class="card__image"
+      :style="{
+        background: `url(${config.public.imageurl}${product.images[0]}) lightgray 50% / cover no-repeat`,
+      }"
+    >
+      <span class="card__discount">-{{ product.discount }}%</span>
+    </div>
+    <div class="card__title">
+      {{ product.name }}
+    </div>
+    <div class="card__price">${{ product.price }}</div>
+  </NuxtLink>
+</template>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  width: 320px;
+  text-decoration: none;
+
+  &__image {
+    aspect-ratio: 1/1;
+    border-radius: 8px;
+    width: 100%;
+    padding: 16px;
+  }
+
+  &__title {
+    font-size: 20px;
+    line-height: 26px;
+    color: var(--color-black);
+  }
+
+  &__price {
+    font-size: 20px;
+    line-height: 26px;
+    font-weight: 500;
+    text-transform: capitalize;
+    color: var(--color-accent);
+  }
+
+  &__discount {
+    background: var(--color-accent);
+    border-radius: 4px;
+    padding: 2px 8px;
+    font-size: 12px;
+    color: var(--color-white);
+  }
+}
+</style>
--- app/pages/catalog/index.vue
+++ app/pages/catalog/index.vue
@@ -1,34 +1,79 @@
 <script setup lang="ts">
+import CatalogCard from "~/components/CatalogCard.vue";
 import VSelect from "~/components/VSelect.vue";
 
-// import type { ICategoriesGET } from "~/interfaces/category.interface";
+import type { ICategoriesGET } from "~/interfaces/category.interface";
 
-// const config = useRuntimeConfig();
-// const API_URL = config.public.apiurl;
+const config = useRuntimeConfig();
+const API_URL = config.public.apiurl;
 const select = ref("");
 
-// try {
-//   const data = await $fetch<ICategoriesGET>(API_URL + "/categories");
-//   console.log(data);
-// } catch (e) {
-//   console.error(e);
-// }
+const { data } = await useFetch<ICategoriesGET>(API_URL + "/categories");
 
-// const { data } = await useFetch<ICategoriesGET>(API_URL + "/categories");
-// console.log(data.value);
+const defaultSelect = { label: "Категории", value: "" };
+const categoriesSelect = computed(() => {
+  return data.value
+    ? data.value.categories
+        .map((c) => ({
+          label: c.name,
+          value: c.id.toString(),
+        }))
+        .concat(defaultSelect)
+    : [defaultSelect];
+});
+
+const testProduct = {
+  id: 1,
+  name: "Lira Earrings",
+  price: 20,
+  short_description: "Элегантные золотистые серьги-кольца",
+  long_description:
+    "Отлично подойдут к любому гардеробу. Чистое золото высокой пробы, которое не оставит вас равнодушными к качеству изделия.",
+  sku: "12",
+  discount: 0,
+  images: [
+    "/images/jewelry/lira1.jpg",
+    "/images/jewelry/lira2.jpg",
+    "/images/jewelry/lira3.jpg",
+    "/images/jewelry/lira4.jpg",
+  ],
+  category_id: 1,
+  category: {
+    id: 1,
+    name: "Серьги",
+    alias: "earrings",
+  },
+};
 </script>
 
 <template>
-  <div>
-    <VSelect
-      v-model="select"
-      :options="[
-        { label: 'Categories', value: '' },
-        { label: 'Odin', value: '1' },
-        { label: 'Dva', value: '2' },
-      ]"
-    />
+  <div class="catalog">
+    <h1 class="catalog__title">Каталог товаров</h1>
+    <div class="catalog__content">
+      <div class="catalog__filter">
+        <VSelect v-model="select" :options="categoriesSelect" />
+      </div>
+      <div class="catalog__cards">
+        <CatalogCard v-bind="testProduct" />
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.catalog {
+  &__title {
+    text-align: left;
+    margin-bottom: 40px;
+  }
+
+  &__content {
+    display: flex;
+    gap: 36px;
+  }
+
+  &__filter {
+    width: 260px;
+  }
+}
+</style>
