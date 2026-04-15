b6ed35d31fff5c3fc4cc6a4f246cbbb19b9c1c66
Pavel <paxa627894@yandex.ru>
2026-04-15 22:16:07 +0500

    update: added routing rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
-    "@nuxt/eslint",
-    "@nuxt/image",
-    "@nuxt/fonts",
-    "@nuxt/scripts",
-    "@nuxt/icon",
-    "@pinia/nuxt",
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxt/scripts',
+    '@nuxt/icon',
+    '@pinia/nuxt',
   ],
+  routeRules: {
+    '/about': { prerender: true },
+    '/': { swr: true },
+    '/catalog/**': { swr: 3600 },
+  },
   icon: {
     customCollections: [
       {
-        prefix: "icons",
-        dir: "./app/assets/icons",
+        prefix: 'icons',
+        dir: './app/assets/icons',
       },
     ],
   },
   runtimeConfig: {
-    token: "",
+    token: '',
     public: {
-      apiurl: "",
-      imageurl: "",
+      apiurl: '',
+      imageurl: '',
     },
   },
   postcss: {
     plugins: {
-      "postcss-nested": {},
+      'postcss-nested': {},
     },
   },
-});
+})
