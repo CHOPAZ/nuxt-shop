4e4becf729c3fbead64fdf62264a601376334780
Pavel <paxa627894@yandex.ru>
2026-04-23 22:41:57 +0500

    update: changes header logo

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -12,9 +12,7 @@
       <div class="header__menu-right">
         <button><Icon name="icons:search" size="21" /></button>
         <NuxtLink to="/basket"><Icon name="icons:basket" size="21" /></NuxtLink>
-        <NuxtLink to="/favorites"
-          ><Icon name="icons:favorites" size="21"
-        /></NuxtLink>
+        <NuxtLink to="/favorites"><Icon name="icons:favorites" size="21" /></NuxtLink>
         <NuxtLink to="/account"><Icon name="icons:user" size="21" /></NuxtLink>
       </div>
     </div>
@@ -22,69 +20,75 @@
 </template>
 
 <style scoped>
-.header {
-  display: flex;
-  margin-top: 48px;
-  justify-content: space-between;
-  border-bottom: 1px solid var(--color-gray);
-}
+  .header {
+    display: flex;
+    margin-top: 48px;
+    justify-content: space-between;
+    border-bottom: 1px solid var(--color-gray);
+  }
 
-.header__logo {
-  width: 100%;
-  max-width: 128px;
-  height: 40px;
-  background-image: url("/assets/image/logo.svg");
-  background-position: center;
-  background-repeat: no-repeat;
-}
+  .header__logo {
+    width: 100%;
+    max-width: 128px;
+    height: 40px;
+    background-image: url('/assets/image/logo.svg');
+    background-position: center;
+    background-repeat: no-repeat;
 
-.header__menu {
-  display: flex;
-}
+    a {
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
+  }
 
-.header__menu-left {
-  display: flex;
-  align-items: center;
-  gap: 60px;
-}
+  .header__menu {
+    display: flex;
+  }
 
-.header__menu-left a {
-  font-size: 16px;
-  line-height: 27px;
-  color: var(--color-black);
-  text-decoration: none;
-  padding-bottom: 20px;
-}
+  .header__menu-left {
+    display: flex;
+    align-items: center;
+    gap: 60px;
+  }
 
-.header__menu-left a.router-link-active {
-  border-bottom: 1px solid var(--color-black);
-}
+  .header__menu-left a {
+    font-size: 16px;
+    line-height: 27px;
+    color: var(--color-black);
+    text-decoration: none;
+    padding-bottom: 20px;
+  }
 
-.header__menu-separator {
-  position: relative;
-  margin: 0 60px;
-}
+  .header__menu-left a.router-link-active {
+    border-bottom: 1px solid var(--color-black);
+  }
 
-.header__menu-separator::after {
-  content: "";
-  position: absolute;
-  top: 8%;
-  width: 1px;
-  height: 17px;
-  background-color: var(--color-dark-gray);
-}
+  .header__menu-separator {
+    position: relative;
+    margin: 0 60px;
+  }
 
-.header__menu-right {
-  display: flex;
-  align-items: center;
-  gap: 40px;
-}
+  .header__menu-separator::after {
+    content: '';
+    position: absolute;
+    top: 8%;
+    width: 1px;
+    height: 17px;
+    background-color: var(--color-dark-gray);
+  }
 
-.header__menu-right a {
-  padding-bottom: 20px;
-}
+  .header__menu-right {
+    display: flex;
+    align-items: center;
+    gap: 40px;
+  }
 
-.header__menu-right a.router-link-active {
-  border-bottom: 1px solid var(--color-black);
-}
+  .header__menu-right a {
+    padding-bottom: 20px;
+  }
+
+  .header__menu-right a.router-link-active {
+    border-bottom: 1px solid var(--color-black);
+  }
 </style>
